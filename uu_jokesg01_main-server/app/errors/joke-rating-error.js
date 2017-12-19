45b8b47edae42b020bd22286d144844302f4b942
Dmytro Romanov <dmytro.romanov@plus4u.net>
2017-12-19 12:38:01 +0200

    error namespace

--- uu_jokesg01_main-server/app/errors/joke-rating-error.js
+++ uu_jokesg01_main-server/app/errors/joke-rating-error.js
@@ -1,47 +1,48 @@
 "use strict";
-const DemoAppError = require("./uu-jokes-error");
-const Code = "addJokeRating/";
+const Errors = require("./uu-jokes-error");
+const AddJokeRatingCode = "addJokeRating";
 
-class InvalidDtoInError extends DemoAppError {
-  setParams() {
-    return {
-      code: `${Code}/invalidDtoIn`,
-      message: "DtoIn is not valid."
-    };
+let addJokeRating = {
+  InvalidDtoIn: class InvalidDtoInError extends Errors.JokesError {
+    setParams() {
+      return {
+        code: `${AddJokeRatingCode}/invalidDtoIn`,
+        message: "DtoIn is not valid."
+      };
+    }
   }
-}
+};
 
-class DaoGetByJokeAndIdentityFailed extends DemoAppError {
-  setParams() {
-    return {
-      code: `${Code}/jokeRatingDaoGetByJokeAndIdentityFailed`,
-      message: "Get jokeRating by Dao getByJokeAndIdentity failed."
-    };
-  }
-}
-
-class DaoUpdateFailed extends DemoAppError {
-  setParams() {
-    return {
-      code: `${Code}/jokeRatingDaoUpdateFailed`,
-      message: "Update jokeRating by Dao update failed."
-    };
-  }
-}
-
-class DaoCreateFailed extends DemoAppError {
-  setParams() {
-    return {
-      code: `${Code}/jokeRatingDaoCreateFailed`,
-      message: "Create jokeRating by Dao create failed."
-    };
-  }
-}
+Errors.addJokeRating = addJokeRating;
 
 module.exports = {
-  Code,
-  InvalidDtoInError,
-  DaoGetByJokeAndIdentityFailed,
-  DaoUpdateFailed,
-  DaoCreateFailed
+  Errors
 };
+
+// ns.addJokeRating.JokeDaoGetFailed = class DaoGetByJokeAndIdentityFailed extends Errors {
+//   setParams() {
+//     return {
+//       code: `${Code}/jokeRatingDaoGetByJokeAndIdentityFailed`,
+//       message: "Get jokeRating by Dao getByJokeAndIdentity failed."
+//     };
+//   }
+// };;
+
+// class DaoUpdateFailed extends Errors {
+//   setParams() {
+//     return {
+//       code: `${Code}/jokeRatingDaoUpdateFailed`,
+//       message: "Update jokeRating by Dao update failed."
+//     };
+//   }
+// }
+
+// class DaoCreateFailed extends Errors {
+//   setParams() {
+//     return {
+//       code: `${Code}/jokeRatingDaoCreateFailed`,
+//       message: "Create jokeRating by Dao create failed."
+//     };
+//   }
+// }
+
--- uu_jokesg01_main-server/app/errors/uu-jokes-error.js
+++ uu_jokesg01_main-server/app/errors/uu-jokes-error.js
@@ -1,28 +1,29 @@
 "use strict";
-const {UseCaseError} = require("uu_appg01_server").AppServer;
-
+const { UseCaseError } = require("uu_appg01_server").AppServer;
 const ERROR_PREFIX = "uu-jokesg01-main/";
 
-class JokesError extends UseCaseError {
-  constructor(dtoOut, paramMap = {}, cause = null) {
-    if (dtoOut instanceof Error) {
-      cause = dtoOut;
-      dtoOut = null;
-      paramMap = {};
+let Errors = {
+  JokesError: class JokesError extends UseCaseError {
+    constructor(dtoOut, paramMap = {}, cause = null) {
+      if (dtoOut instanceof Error) {
+        cause = dtoOut;
+        dtoOut = null;
+        paramMap = {};
+      }
+
+      super({
+        dtoOut: dtoOut,
+        paramMap: paramMap,
+        status: 400,
+        cause: cause
+      });
+
+      let errParams = this.setParams();
+      this.message = errParams.message;
+      this.code = ERROR_PREFIX + errParams.code;
+      errParams.status && (this.status = errParams.status);
     }
-
-    super({
-      dtoOut: dtoOut,
-      paramMap: paramMap,
-      status: 400,
-      cause: cause
-    });
-
-    let errParams = this.setParams();
-    this.message = errParams.message;
-    this.code = ERROR_PREFIX + errParams.code;
-    errParams.status && (this.status = errParams.status);
   }
-}
+};
 
-module.exports = JokesError;
+module.exports = Errors;
