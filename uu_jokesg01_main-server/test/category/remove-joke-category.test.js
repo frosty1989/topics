d5a02f818550f3ce6d615152322897fe40599368
Denys Barabash <20-6544-1@plus4u.net>
2018-01-02 16:42:00 +0200

    Fix remove-joke-category test.

--- uu_jokesg01_main-server/test/category/remove-joke-category.test.js
+++ uu_jokesg01_main-server/test/category/remove-joke-category.test.js
@@ -1,36 +1,38 @@
-const {Utils} = require("uu_appg01_server");
-const {TestHelper} = require("uu_appg01_workspace-test");
+const { TestHelper } = require("uu_appg01_workspace-test");
+const { CreateCategory } = require("../general-test-hepler");
+const { CreateJoke } = require("../general-test-hepler");
 
-beforeEach(async (done) => {
+beforeEach(async done => {
   await TestHelper.setup();
   await TestHelper.initAppWorkspace();
   await TestHelper.createPermission("Readers");
   done();
 });
 
-afterEach(async (done) => {
+afterEach(async done => {
   await TestHelper.teardown();
   done();
 });
 
-//Happy day scenario
 describe("Test removeJokeCategory command", () => {
-  test("test the deleteCategory method", async () => {
+  test("HDS", async () => {
     await TestHelper.login("Readers");
-
-    let newJoke = await TestHelper.executePostCommand("createJoke", {
-      name: "test name",
-      text: "test desc",
-      categoryList: ["e001", "e001"]
-    });
-    let dtoInForAddJokeCategory = { jokeId: newJoke.data.id, categoryList: ["e001", "e003"] };
+    let category = await CreateCategory();
+    let categoryId = category.id;
+    let joke = await CreateJoke({}, categoryId);
+    console.log(joke);
+    let jokeId = joke.data.id;
+    let dtoInForAddJokeCategory = {
+      jokeId: jokeId,
+      categoryList: [categoryId]
+    };
     let newJokeCategory = await TestHelper.executePostCommand("addJokeCategory", dtoInForAddJokeCategory);
-    let jokeId = newJokeCategory.data.jokeId;
     let categories = newJokeCategory.data.categoryList;
     let response = await TestHelper.executePostCommand("removeJokeCategory", {
       jokeId: jokeId,
       categoryList: categories
     });
+
     expect(response.status).toEqual(200);
     expect(response.data).toBeDefined();
     expect(response.data.uuAppErrorMap).toBeDefined();
@@ -40,52 +42,84 @@ describe("Test removeJokeCategory command", () => {
 });
 
 describe("Test removeJokeCategory command", () => {
-  test("test for invalid keys", async () => {
+  test("A1", async () => {
     await TestHelper.login("Readers");
 
-    let newJoke = await TestHelper.executePostCommand("createJoke", {
-      name: "test name",
-      text: "test desc",
-      categoryList: ["e001", "e001"]
-    });
-    let dtoInForAddJokeCategory = { jokeId: newJoke.data.id, categoryList: ["e001", "e003"] };
-    let newJokeCategory = await TestHelper.executePostCommand("addJokeCategory", dtoInForAddJokeCategory);
-    let jokeId = newJokeCategory.data.jokeId;
+    let category = await CreateCategory();
+    let categoryId = category.id;
+    let joke = await CreateJoke({}, categoryId);
+    console.log(joke);
+    let jokeId = joke.data.id;
+    let dtoInForAddJokeCategory = {
+      jokeId: jokeId,
+      categoryList: [categoryId]
+    };
+    let newJokeCategory = await TestHelper.executePostCommand(
+      "addJokeCategory",
+      dtoInForAddJokeCategory
+    );
     let categories = newJokeCategory.data.categoryList;
     let response = await TestHelper.executePostCommand("removeJokeCategory", {
       jokeId: jokeId,
       categoryList: categories,
       invalid: "invalid"
     });
-    expect(typeof(response.data.uuAppErrorMap)).toBe("object");
+    expect(typeof response.data.uuAppErrorMap).toBe("object");
     expect(response.data.uuAppErrorMap).toBeInstanceOf(Object);
-    expect("warning").toEqual(response.data.uuAppErrorMap['uu-jokesg01-main/removeJokeCategory/unsupportedKey'].type);
-    expect("DtoIn contains unsupported keys.").toEqual(response.data.uuAppErrorMap['uu-jokesg01-main/removeJokeCategory/unsupportedKey'].message);
-    let invalidData = response.data.uuAppErrorMap['uu-jokesg01-main/removeJokeCategory/unsupportedKey'].paramMap['unsupportedKeyList'][0];
-    expect(invalidData).toEqual('$.invalid');
+    expect("warning").toEqual(
+      response.data.uuAppErrorMap[
+        "uu-jokesg01-main/removeJokeCategory/unsupportedKey"
+      ].type
+    );
+    expect("DtoIn contains unsupported keys.").toEqual(
+      response.data.uuAppErrorMap[
+        "uu-jokesg01-main/removeJokeCategory/unsupportedKey"
+      ].message
+    );
+    let invalidData =
+      response.data.uuAppErrorMap[
+        "uu-jokesg01-main/removeJokeCategory/unsupportedKey"
+      ].paramMap["unsupportedKeyList"][0];
+    expect(invalidData).toEqual("$.invalid");
   });
 });
 
 describe("Test removeJokeCategory command", () => {
   test("if joke category does not exist", async () => {
     await TestHelper.login("Readers");
-    let newJoke = await TestHelper.executePostCommand("createJoke", {
-      name: "test name",
-      text: "test desc",
-      categoryList: ["e001", "e001"]
-    });
-    let dtoInForAddJokeCategory = { jokeId: newJoke.data.id, categoryList: ["e001", "e003"] };
-    let newJokeCategory = await TestHelper.executePostCommand("addJokeCategory", dtoInForAddJokeCategory);
+    let category = await CreateCategory();
+    let categoryId = category.id;
+    let joke = await CreateJoke({}, categoryId);
+    console.log(joke);
+    let jokeId = joke.data.id;
+    let dtoInForAddJokeCategory = {
+      jokeId: jokeId,
+      categoryList: [categoryId]
+    };
+    let newJokeCategory = await TestHelper.executePostCommand(
+      "addJokeCategory",
+      dtoInForAddJokeCategory
+    );
     let categories = newJokeCategory.data.categoryList;
-    let status;
-    try{
+    let response = await TestHelper.executePostCommand("removeJokeCategory", {
+      jokeId: jokeId,
+      categoryList: categories,
+      invalid: "invalid"
+    });
+    let result;
+    try {
       await TestHelper.executePostCommand("removeJokeCategory", {
-        jokeId: 123,
-        categoryList: categories,
+        wrongKey: 123,
+        categoryList: "Wrong value"
       });
-    } catch(error) {
-      status = error.response.status;
+    } catch (error) {
+      result = error;
     }
-    expect(status).toBe(500);
+    expect(result.status).toEqual(400);
+    expect(result.code).toBeDefined();
+    expect(result.code).toBe("uu-jokesg01-main/removeJokeCategory/invalidDtoIn");
+    expect(result.paramMap).toBeDefined();
+    expect(result.paramMap.invalidValueKeyMap).toBeDefined();
+    expect(result.paramMap.missingKeyMap).toBeDefined();
   });
 });
