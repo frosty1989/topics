72a46cb973374c6af37881c12414b9065e480ea4
Vojtech Sebek <vojtech.sebek@plus4u.net>
2018-12-14 10:51:32 +0100

    change http method of jokesInstance/load

--- uu_jokes_maing01-server/test/jokes_instance/jokes-instance-load.test.js
+++ uu_jokes_maing01-server/test/jokes_instance/jokes-instance-load.test.js
@@ -19,7 +19,7 @@ beforeEach(async () => {
 test("HDS", async () => {
   await TestHelper.executePostCommand(JOKES_INSTANCE_INIT, { uuAppProfileAuthorities: "." });
   await TestHelper.login("Authority");
-  let result = await TestHelper.executePostCommand(JOKES_INSTANCE_LOAD);
+  let result = await TestHelper.executeGetCommand(JOKES_INSTANCE_LOAD);
   expect(result.status).toBe(200);
   let dtoOut = result;
   expect(dtoOut.state).toEqual("underConstruction");
@@ -29,11 +29,11 @@ test("HDS", async () => {
   expect(dtoOut.authorizedProfileList).toEqual(["Authorities"]);
 });
 
-test("A1 - JOKES_INSTANCE_LOADing non existent jokes instance", async () => {
+test("A1 - loading non existent jokes instance", async () => {
   expect.assertions(2);
   await TestHelper.login("Authority");
   try {
-    await TestHelper.executePostCommand(JOKES_INSTANCE_LOAD);
+    await TestHelper.executeGetCommand(JOKES_INSTANCE_LOAD);
   } catch (e) {
     expect(e.code).toEqual("uu-jokes-main/jokesInstance/load/jokesInstanceDoesNotExist");
     expect(e.message).toEqual("JokesInstance does not exist.");
@@ -45,7 +45,7 @@ test("A2 - closed jokes instance", async () => {
   await TestHelper.executePostCommand(JOKES_INSTANCE_INIT, { uuAppProfileAuthorities: ".", state: "closed" });
   await TestHelper.login("Authority");
   try {
-    await TestHelper.executePostCommand(JOKES_INSTANCE_LOAD);
+    await TestHelper.executeGetCommand(JOKES_INSTANCE_LOAD);
   } catch (e) {
     expect(e.code).toEqual("uu-jokes-main/jokesInstance/load/jokesInstanceNotInProperState");
     expect(e.message).toEqual("JokesInstance is not in proper state [active|underConstruction].");
@@ -62,7 +62,7 @@ test("A3 - jokes instance is under construction and caller is a Reader", async (
   });
   await TestHelper.login("Reader");
   try {
-    await TestHelper.executePostCommand(JOKES_INSTANCE_LOAD);
+    await TestHelper.executeGetCommand(JOKES_INSTANCE_LOAD);
   } catch (e) {
     expect(e.code).toEqual("uu-jokes-main/jokesInstance/load/jokesInstanceIsUnderConstruction");
     expect(e.message).toEqual("JokesInstance is in state underConstruction.");
